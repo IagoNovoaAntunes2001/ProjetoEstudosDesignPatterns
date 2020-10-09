b65199e92b677a1b39e5a8ce482a76314f33538f
Iago Novoa Antunes <inovoaan@everis.com>
2020-10-09 18:04:14 -0300

    Passando comentarios para classes de abstracoes

--- app/src/main/java/com/example/myapplication/Solid/O/Pagamento.kt
+++ app/src/main/java/com/example/myapplication/Solid/O/Pagamento.kt
@@ -1,5 +1,11 @@
 package com.example.myapplication.Solid.O
 
+// OCP nos informa que  devemos ser capazes de extender um comportamento de classe, sem modificá-lo.
+
+//devemos ser capazer de extender o funcionamento da nossa classe de pagamento ou seja "pagar",
+//porem sem modificar a classe existente.
+
+
 abstract class Pagamento {
     open var valor = 20
     abstract fun pagar()
--- app/src/main/java/com/example/myapplication/Solid/S/ContaCorrente.kt
+++ app/src/main/java/com/example/myapplication/Solid/S/ContaCorrente.kt
@@ -1,16 +1,8 @@
 package com.example.myapplication.Solid.S
 
-// "Uma classe deve ter apenas um motivo para mudar"
-// Sua responsabilidade é sempre em sima de um e apenas um ator.
-
-// Um objeto não pode e deve fazer mais do que é o dominio dele.
-// Lembre-se: sua funcionalidade não importa a granularidade (pacote/modulo/classe/metodo/função)
-// não deve fazer o que não é proposto, normalmente um Funcionario não deve ser capaz de adicionar um produto em seu dominio,
-// um Produto não deve ser capaz de exercer controle sobre o banco de dados, um pacote que diz que faz A não deve fazer B.
-
 class ContaCorrente(
     private val contaCorrente: ContaCorrenteRepository
-){
+) {
     fun salvarModificacao() {
         contaCorrente.save()
     }
